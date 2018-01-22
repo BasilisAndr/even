4111ae9b2b8e065836853d22aff015aa88b31e67
BasilisAndr <blindedbysunshine@gmail.com>
2018-01-22 20:31:08 +0300

    trying mixed effects and anova??

--- mixed_effects.r
+++ mixed_effects.r
@@ -2,11 +2,13 @@ data <- read.csv('~/Documents/Langs/Even/apply/corpora_by_speakers.csv', strings
 summary(data)
 all.morphs <- unique(data$morpheme)
 lms <- c()
+aovs <- c()
 morphs <- c()
-for(mor in all.morphs) {
+for(mor in all.morphs[1:5]) {
   ds <- data[data$morpheme == mor, ];
   if(length(unique(ds$corpus))==2){
   lms <- c(lms, lm(ds$count~ds$corpus+ds$speaker));
+  aovs <- c(aovs, aov(ds$count~ds$corpus))
   morphs <- c(morphs, mor)}
 }
 
@@ -14,3 +16,5 @@ print(length(all.morphs))
 print(length(morphs))
 
 lms[1]
+aovs[1]
+aov(ds$count~ds$corpus)
